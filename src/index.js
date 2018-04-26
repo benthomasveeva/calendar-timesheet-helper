85aeb1441d475f96218bc84144ba9dda9aae498e
Ben Thomas <ben.thomas@veeva.com>
2018-04-26 15:07:06 -0400

    Avoid a feedback loop when .hint() also fails

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.ports.elmToJs.subscribe((elmMsg) => {
             app.ports.jsToElm.send({ "msg": "credentialsuccess", "idToken": credential.idToken, "email": credential.id });
         }, (error) => {
             console.log("error", error);
-            app.ports.jsToElm.send({ "msg": "credentialfail", "error": error });
+            // app.ports.jsToElm.send({ "msg": "credentialfail", "error": error });
         });
     } else if (elmMsg.msg === "loadapi") {
         gapi.load('client', () => {
